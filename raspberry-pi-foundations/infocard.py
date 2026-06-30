e827197fd652baf69223e06f34b5b61eb44696c6
Kerstin <mimikegates24@gmail.com>
2026-06-30 14:24:17 -0400

    I added info

---
+++ raspberry-pi-foundations/infocard.py
@@ -0,0 +1,17 @@
+name = "kerstin gatson-smith"
+age = "10th grade"
+
+
+
+interest ="I love to play soccer, I also love a good game like anything, a console I love playing on would be vr"
+why_here = "I am here because I was it to be a good sign for when I sign up for the astronaut position"
+goals = "I want to go to space and I need I want to learn more skills"
+personality = "curiosity, advaturouse, sleepy, hungry, happy, energatic,"
+
+print = ("name", name)
+print("Age/Grade", age)
+
+print("\nInterests:", interest)
+print("Why I'm here:", why_here)
+print("Goals:", goals)
+print("personality:", personality)
