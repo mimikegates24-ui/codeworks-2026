239b6fa00f5e488dacc622ea89b55c00317eeab5
Kerstin <mimikegates24@gmail.com>
2026-07-07 14:40:40 -0400

    I like foxes

--- raspberry-pi-foundations/infocard.py
+++ raspberry-pi-foundations/infocard.py
@@ -3,7 +3,7 @@ age = "10th grade"
 
 
 
-interest ="I love to play soccer, I also love a good game like anything, a console I love playing on would be vr"
+interest ="I ABSOLUTLY LOVE SPACE I love to play soccer, I also love a good game like anything, a console I love playing on would be vr"
 why_here = "I am here because I was it to be a good sign for when I sign up for the astronaut position"
 goals = "I want to go to space and I need I want to learn more skills"
 personality = "curiosity, advaturouse, sleepy, hungry, happy, energatic,"
